52fe49234567452dfdc16158b6da8526fdced9d7
Oscar Gutierrez <oscar.gutierrezr@udea.edu.co>
2023-08-28 17:13:55 -0500

    Solucionados todos los errores, falta configurar pines y probar en FPGA

--- Practica_I/fsm.sv
+++ Practica_I/fsm.sv
@@ -2,40 +2,26 @@ module fsm (clk, rst, up, value);
 	input logic clk, up, rst;
 	output logic [3:0] value;
 
-	int position = 1;
+	int position = 0;
 	localparam logic [3:0] array [0:7] = '{4, 8, 12, 0, 3, 7, 11, 15};
 
 	always_ff @(posedge clk, posedge rst) begin
 		
 		if (rst) begin
-			position <= 1;
+			position <= 0;
 		end
 		
-//		else if (up) begin
-//			if(position < 7 & position > 0) begin
-//				//value <= array[position];
-//				position <= position + 1;
-//			end else
-//				position <= 0;
-//				//value <= 4'b0100;
-//		end else begin
-//			if(position < 7 & position > 0) begin
-//				//value <= array[position];
-//				position <= position - 1;
-//			end else
-//				position <= 0;
-//			//value <= 4'b1111;
-//			end
-		
-		else if (position < 7 & position > 0) begin
-			if(up) begin
+		else if (up) begin
+			if(position < 7) begin
 				position <= position + 1;
-			end else begin
-				position <= position - 1;
-			end
+			end else
+				position <= 0;
 		end else begin
-			position <= 1;
-		end
+			if(position > 0) begin
+				position <= position - 1;
+			end else
+				position <= 7;
+			end
 		
 		value <= array[position];
 		
