92a9ac3bad1d95750104c215e1c956e7389d0705
Oscar Gutierrez <oscar.gutierrezr@udea.edu.co>
2023-08-28 16:55:53 -0500

    Se creó el testbench, se corrigieron errores, pero aún hay problemas con la máquina de estados

--- Practica_I/fsm.sv
+++ Practica_I/fsm.sv
@@ -2,27 +2,44 @@ module fsm (clk, rst, up, value);
 	input logic clk, up, rst;
 	output logic [3:0] value;
 
-	integer position = 0;
-	logic [3:0] array [0:7] = '{4, 8, 12, 0, 3, 7, 11, 15};
+	int position = 1;
+	localparam logic [3:0] array [0:7] = '{4, 8, 12, 0, 3, 7, 11, 15};
 
 	always_ff @(posedge clk, posedge rst) begin
 		
 		if (rst) begin
-			position = 0;
+			position <= 1;
 		end
 		
-		else if (up) begin
-			if(position <= 7) begin
-				value = array[position];
-				position ++;
-			end else
-				position = 0;
-		end else
-			if(position >= 0) begin
-				value = array[position];
-				position --;
-			end else
-				position = 7;
+//		else if (up) begin
+//			if(position < 7 & position > 0) begin
+//				//value <= array[position];
+//				position <= position + 1;
+//			end else
+//				position <= 0;
+//				//value <= 4'b0100;
+//		end else begin
+//			if(position < 7 & position > 0) begin
+//				//value <= array[position];
+//				position <= position - 1;
+//			end else
+//				position <= 0;
+//			//value <= 4'b1111;
+//			end
+		
+		else if (position < 7 & position > 0) begin
+			if(up) begin
+				position <= position + 1;
+			end else begin
+				position <= position - 1;
+			end
+		end else begin
+			position <= 1;
 		end
+		
+		value <= array[position];
+		
+		end
+		
 endmodule
 
--- Practica_I/main.sv
+++ Practica_I/main.sv
@@ -1,31 +1,69 @@
-module main(
+module main # (parameter fpga_freq = 50_000_000)
+(
 	
 	input logic up,
 	input logic rst,
 	input logic clk,
 	input logic timer,
-	output logic [5:0] disp,
-	output logic [6:0] segm
+	output logic [6:0] decsegm, unisegm
 );
 
 
 	reg internalClk;
 	logic [3:0] value, tens, units;// número que se mostrará en el display sin su separación
-	logic [6:0] decenas, unidades;
 
 
 	
-cntdiv_n clock (clk, rst, internalClk, timer);
+cntdiv_n #(fpga_freq) clock (clk, ~rst, internalClk, timer);
 
-fsm machine (internalClk, rst, up, value);
+fsm machine (internalClk, ~rst, up, value);
 
 converter conversor (value, tens, units);
 
-deco7seg deco1 (tens, decenas);
+deco7seg deco1 (tens, decsegm);
 
-deco7seg deco2 (units, unidades);
+deco7seg deco2 (units, unisegm);
 	
 endmodule
 
 
 //¿Cómo hacer test Bench?
+
+module tbmain ();
+
+localparam CLK_PERIOD = 20ns;
+localparam fpga_freq = 8;
+logic up;
+logic rst;
+logic clk;
+logic timer;
+logic [6:0] decsegm;
+logic [6:0] unisegm;
+
+main #(fpga_freq) main_u0 (up, rst, clk, timer, decsegm, unisegm);
+
+initial begin
+
+clk = 0;
+rst = 0;
+up = 1;
+timer = 0;
+
+#(CLK_PERIOD * 1);
+rst = 1;
+
+#(CLK_PERIOD * 40);
+up = 0;
+
+#(CLK_PERIOD * 80);
+timer = 1;
+up =1;
+
+#(CLK_PERIOD * 120);
+
+$stop;
+end
+
+always #(CLK_PERIOD / 2) clk = ~clk;
+
+endmodule
