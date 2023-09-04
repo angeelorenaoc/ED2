20f9d8855fd779e7d4f7bc4a3c5d333f4dd766dc
Oscar Gutierrez <oscar.gutierrezr@udea.edu.co>
2023-09-04 15:25:59 -0500

    Se creó el módulo main y se inició el test bench

--- Practica_II/deco7seg.sv
+++ Practica_II/deco7seg.sv
@@ -1,28 +1,32 @@
 module deco7seg_hexa(
-	input  logic [3:0] D,
+	input  logic [3:0] D, tens,
+	input logic Flag, 
+	input int deco,
 	output logic [6:0] SEG
 );
  
 	always_comb begin
-		case(D)
-								// gfedcba
-			4'b0000: SEG = 7'b1000000; // 0x40
-			4'b0001: SEG = 7'b1111001; // 0x79
-			4'b0010: SEG = 7'b0100100; // 0x24
-			4'b0011: SEG = 7'b0110000; // 0x30
-			4'b0100: SEG = 7'b0011001; // 0x19
-			4'b0101: SEG = 7'b0010010; // 0x12 
-			4'b0110: SEG = 7'b0000010; // 0x02
-			4'b0111: SEG = 7'b1111000; // 0x78
-			4'b1000: SEG = 7'b0000000; // 0x00
-			4'b1001: SEG = 7'b0011000; // 0x18
-			4'b1010: SEG = 7'b0001000; // 0x08
-			4'b1011: SEG = 7'b0000011; // 0x03
-			4'b1100: SEG = 7'b1000110; // 0x46
-			4'b1101: SEG = 7'b0100001; // 0x30
-			4'b1110: SEG = 7'b0000110; // 0x06
-			4'b1111: SEG = 7'b0001110; // 0x0E
-			default: SEG = 7'b1111111;
-		endcase
+		if ((tens==0 & Flag == 1 & deco == 1) or (tens!=0 & Flag == 1 & deco == 1)) begin
+			SEG = 7'b0111111;
+		end
+		else if ((tens==0 & Flag == 0 & deco == 1) or (tens==0 & Flag == 0 & deco == 0) OR (tens==0 & Flag == 1 & deco == 0))begin
+			SEG = 7'b1111111;
+		end
+		else begin
+			case(D)
+									// gfedcba
+				4'b0000: SEG = 7'b1000000;
+				4'b0001: SEG = 7'b1111001;
+				4'b0010: SEG = 7'b0100100;
+				4'b0011: SEG = 7'b0110000;
+				4'b0100: SEG = 7'b0011001;
+				4'b0101: SEG = 7'b0010010; 
+				4'b0110: SEG = 7'b0000010;
+				4'b0111: SEG = 7'b1111000;
+				4'b1000: SEG = 7'b0000000;
+				4'b1001: SEG = 7'b0011000;
+				default: SEG = 7'b1111111;
+			endcase
+		end
 	end
 endmodule
--- Practica_II/main.sv
+++ Practica_II/main.sv
@@ -8,10 +8,56 @@ module main #(PARAMETER Bits = 5)
 	);
 	
 	logic control, Result;
+	logic [3:0] ten, unit
 	
-	counterpulse oper(Opersel, clk, rst control);
-	ALU alu(vA, vB,control,Result,ALUFlags);
+	counterpulse oper(~Opersel, clk, rst control);
+	
+	ALU (Bits) alu(vA, vB,control,Result,ALUflags);
+	
+	converter #(Bits)(ALUflags[3],Result, ten, unit)
+	
+		input  logic [3:0] D, tens,
+	input logic Flag, deco,
+	output logic [6:0] SEG
+	
+	deco7segs decounit(unit, ten, ALUflags[3], 2, units)
+	
+	deco7segs decodec(ten, ten, ALUflags[3], 1, tens)
+	
+	deco7segs decominus(0, ten, ALUflags[3], 0, minusv)
 	
 	
+	endmodule
+	
+	module tbmain ();
+	
+	localparam CLK_PERIOD = 20ns;
+	localparam Bits = 5;
+	local clk;
+	local rst;
+	local Opersel;
+	local [Bits-1:0] vA;
+	local [Bits-1:0] vB;
+	local [6:0] minus;
+	local [6:0] tens;
+	local [6:0] units;
+	local [3:0] ALUflags;
+	local [3:0] operation;
+	
+	main (5)(clk,rst, Opersel, vA, vB, minus, tens, units, ALUflags, operation);
+	
+	initial begin
+	
+	clk = 0;
+	rst = 1;
+	vA = 5'b01001;
+	vB = 5'b11010;
+	Opersel = 0;
+	
+	for (int i = 0, i<4, i++) begin
+		for (int j = 0, j<4, j++) begin
+			Opersel=1;
+		end
+	end
 	
 	endmodule
