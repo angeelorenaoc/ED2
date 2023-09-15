cd9c4eed77503e71fdedc2bf7715e6f02a6152dd
unknown <angee.ocampo@udea.edu.co>
2023-09-15 11:53:16 -0500

    Se organizó la máquina de estados, se instanciaron distintos módulos de la unidad de periféricos, y se generó el contador que controla las entradas

--- Practica_III/peripheral_getoperands.sv
+++ Practica_III/peripheral_getoperands.sv
@@ -7,14 +7,12 @@
 module peripheral_getoperands (clk, reset, inputdata, enterpulse, datainput_i, dataA, dataB);
 	input logic clk, reset;
 	input logic [7:0] inputdata;
-	input logic enterpulse;
-	//input logic [3:0] datainput_i;
-	output logic [3:0] datainput_i;
+	input logic enterpulse;//¿Por qué se usa el enterpulse?
+	input logic [3:0] datainput_i;
+	//output logic [3:0] datainput_i;
 	output logic [31:0] dataA, dataB;
 
 	// Internal signals and processes to store data into proper registers
-	logic pulse;
-	int count;
 	logic [63:0] aux;
 	
 	peripheral_pulse pulse0 (enterpulse, clk, reset, pulse);
@@ -23,10 +21,15 @@ module peripheral_getoperands (clk, reset, inputdata, enterpulse, datainput_i, d
 		if(reset) begin
 			datainput_i <= 0;
 		end
-		else if (datainput_i < 8) begin//Revisar mañana condicionar aumento al pulso
-			aux[datainput_i*8+:8] <= inputdata;
-			datainput_i <= datainput_i + 1'b1;
+		else if(enterpulse)begin 
+			if (datainput_i < 8) begin//Revisar mañana condicionar aumento al pulso
+				aux[datainput_i*8+:8] <= inputdata;
+				//datainput_i <= datainput_i + 1'b1;
+			end
 		end
 	end
+	
+	assign dataA = aux[31:0];
+	assign dataB = aux[63:32];
 	
 endmodule			
