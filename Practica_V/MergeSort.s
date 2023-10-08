c681d6b6472905d958ed926b9557967c8d1de1b2
unknown <angee.ocampo@udea.edu.co>
2023-10-08 15:34:44 -0500

    Creación de carpeta y plantilla Practica 5

---
+++ Practica_V/MergeSort.s
@@ -0,0 +1,18 @@
+.global _start
+	.equ MAXN, 200
+	.text 
+_start:
+	
+finish:
+	b finish
+
+MergeSort://Función que segmenta la lista
+
+Fuse://Función que organiza los números
+	
+	
+.data
+
+N:  		.dc.l
+Data: 		.dc.l
+SortedData: .ds.l MAXN
