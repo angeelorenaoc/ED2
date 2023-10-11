509ca60721ce241a3b6cba2bdfb48f09edca1889
unknown <angee.ocampo@udea.edu.co>
2023-10-11 15:07:36 -0500

    Se hace el fuse, corregir errores

--- Practica_V/MergeSort.s
+++ Practica_V/MergeSort.s
@@ -1,20 +1,24 @@
 .global _start
 	.equ MAXN, 200
-	.text 
+	.text //EL LR NO CAMBIA
 _start:
+	LDR R12,=Data
 	LDR R0,=SortedData 	//Dirección de la primera posición del arreglo de salida
-	LDR R12,=SortedData
 	MOV R1, #0				//Valor de i
 	LDR R2, =N
+	LDR R7, [R2]
 	LDR R2, [R2], #-1  		//Valor de j que indica la longitud del arreglo menos 1
+	LDR R6, [R12]	
 	
-/*Copia:					//Copia de la entrada en la salida
+Copia_Original:				//Copia de la entrada en la salida
 	STR R6, [R0], #4
-	LDR R6, [R5,#4]!
+	LDR R6, [R12,#4]!
 	SUBS R7, R7, #1
-	BNE Copia
-	LDR R0, =SortedData*/
+	BNE Copia_Original
+	LDR R0, =SortedData
+	LDR R12, =C
 	
+BL MergeSort
 
 finish:
 	b finish
@@ -41,6 +45,12 @@ MergeSort:				//Función que segmenta la lista
 		POP {R1,LR}		//Recupera el valor orginal de i
 		BL Fuse
 		
+		LDR R7, =N
+		MOV R5, R2	
+		BL Copia_C
+		
+		MOV PC, LR
+		
 
 Fuse://Función que organiza los números
 	While:
@@ -57,17 +67,47 @@ Fuse://Función que organiza los números
 		CMP R8, R9 					//s[p] < s[q]
 		BGE ELSE_FUSE
 		IF_FUSE:
-		
+			STR R8, [R12, R7, LSL #2]
+			ADD R5, R5, #1
+			B While
 		ELSE_FUSE:
+			STR R9, [R12, R7, LSL #2]
+			ADD R6, R6, #1
+			B While
+			
+		CMP R5, R3
+		BGT While_2
 		
+		While_1:
+			CMP R5, R3
+			BEQ End_While
+			STR R8, [R12, R7, LSL #2]
+			ADD R5, R5, #1
+			B While_1
 		
+		While_2:
+			CMP R6, R2
+			BEQ End_While
+			STR R9, [R12, R7, LSL #2]
+			ADD R6, R6, #1
+			B While_2	
+			
 	End_While:
 		MOV PC,LR
-		
-	//While 2?
 	
+	
+Copia_C:				//Copia el arreglo C en la salida
+	LDR R8, [R12], #4
+	STR R8, [R0], #4
+	SUBS R7, R7, #1
+	BNE Copia_C
+	LDR R0, =SortedData
+	LDR R12, =C
+	MOV PC, LR
+
 .data
 
 N:  		.dc.l 10
 Data: 		.dc.l 0x01226a27, 0xb7bc3585, 0xd33eba1d, 0x08563ea1, 0x41129fe2, 0xf716aedb, 0x2e281c6a, 0x4c27210a, 0xb4e92888, 0xdda93c9c
 SortedData: .ds.l MAXN
+C: 			.ds.l MAXN
