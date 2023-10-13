0c2870038ab9aa6ef6b0c68d5b6eb94c02522d29
unknown <angee.ocampo@udea.edu.co>
2023-10-13 14:10:26 -0500

    Ordena las listas, falta hacer más pruebas

--- Practica_V/MergeSort.s
+++ Practica_V/MergeSort.s
@@ -7,7 +7,8 @@ _start:
 	MOV R1, #0				//Valor de i
 	LDR R2, =N
 	LDR R7, [R2]
-	LDR R2, [R2], #-1  		//Valor de j que indica la longitud del arreglo menos 1
+	LDR R2, [R2,#0]  		//Valor de j que indica la longitud del arreglo menos 1
+	SUB R2, R2, #1
 	LDR R6, [R12]	
 	MOV SP, #0
 	
@@ -38,21 +39,22 @@ MergeSort:				//Función que segmenta la lista
 	ELSE:
 		ADD R3,R1,R2
 		LSR R3, R3, #1 		//Se cálcula el nuevo punto medio para el arreglo	
-		PUSH {R2} 	//Guarda el valor de j y lo reemplza por m
+		PUSH {R2,R3} 	//Guarda el valor de j y lo reemplza por m
 		MOV R2, R3
 		BL  MergeSort
 		MOV R3, R2 		//Recupera el valor original de m
-		POP {R2}	//Recupera el valor original de j
-		MOV R4, R3  //ESTO ES UN INTENTO VAGO DE NO PERDR EL VALOR DE R3
-		ADD R3, R3, #1 	//Cálcula m+1
-		PUSH {R1} 	//Guarda el valor de i y lo reemplaza por m
-		MOV R1, R3		
+		POP {R2,R3}	//Recupera el valor original de j
+		/*MOV R4, R3  //ESTO ES UN INTENTO VAGO DE NO PERDR EL VALOR DE R3
+		ADD R3, R3, #1 	//Cálcula m+1*/
+		PUSH {R1,R3} 	//Guarda el valor de i y lo reemplaza por m
+		MOV R1, R3
+		ADD R1, R1, #1		
 		BL  MergeSort
-		POP {R1}		//Recupera el valor orginal de i
+		POP {R1, R3}		//Recupera el valor orginal de i
 		BL Fuse
 		
 		//LDR R7, =N
-		MOV R5, R2	
+		//MOV R5, R2	
 		//BL Copia_C
 		B Copia_C
 		Return_C:
@@ -62,12 +64,12 @@ MergeSort:				//Función que segmenta la lista
 		
 
 Fuse://Función que organiza los números
+	PUSH {R4, R5, R6, R7, R8, R9}
+	MOV R5, R1 					//Contador p
+	MOV R6, R3 					//Contador q
+	ADD R6, R6, #1
+	MOV R7, R1 					//Contador r
 	While:
-		PUSH {R4, R5, R6, R7, R8, R9}
-		MOV R5, R1 					//Contador p
-		ADD R4, R4, #1
-		MOV R6, R4 					//Contador q
-		MOV R7, R1 					//Contador r
 		CMP R5, R3
 		BGT COMP
 		CMP R6, R2
@@ -93,7 +95,7 @@ Fuse://Función que organiza los números
 		
 		While_1:
 			CMP R5, R3
-			BEQ End_While
+			BHI End_While
 			LDR R8, [R0, R5, LSL #2]	//s[p]
 			STR R8, [R12, R7, LSL #2]
 			ADD R5, R5, #1
@@ -102,7 +104,7 @@ Fuse://Función que organiza los números
 		
 		While_2:
 			CMP R6, R2
-			BEQ End_While
+			BHI End_While
 			LDR R9, [R0, R6, LSL #2]	//s[q]
 			STR R9, [R12, R7, LSL #2]
 			ADD R6, R6, #1
@@ -120,7 +122,7 @@ Copia_C:				//Copia el arreglo C en la salida
 		LDR R8, [R12], #4
 		STR R8, [R0], #4
 		SUBS R2, R2, #1
-		BNE For
+		BHS For
 	LDR R0, =SortedData
 	LDR R12, =C
 	POP {R2}
@@ -129,7 +131,13 @@ Copia_C:				//Copia el arreglo C en la salida
 	
 .data
 
-N:  		.dc.l 3
-Data: 		.dc.l 0x01226a27, 0xb7bc3585, 0xd33eba1d
+N:  		.dc.l 	10
+//Data: 		.dc.l  0x9, 0x12, 0x4, 0x30, 0x22, 0x6, 0x43
+Data: 		.dc.l   0x7c5b8d6a, 0x9c7e3d5f, 0x4b7a9c2e, 0x6f1d8c5a, 0x3e9a4b7d, 0x8f2c1b3e, 0x5d8e9a4b, 0x2e6f1d8c, 0x1b3e9a4b, 0x9c2e6f1d
+					/*
+					2.086.374.762 (10), -1.669.448.353 (3), 1.266.326.574(7), 1.864.207.450 (9), 1.050.299.261 (6), -1.892.934.850 (1), 1.569.626.699 (8), 779.033.996 (5), 457.087.563 (4),
+					-1.674.678.499(2)
+					*/
 SortedData: .ds.l MAXN
 C: 			.ds.l MAXN
+	
